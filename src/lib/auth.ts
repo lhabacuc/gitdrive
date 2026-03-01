7b96d659c5c70292627b0f78084a4257a042d8c2
lhabacuc <lisvaldosf@gmail.com>
2026-03-01 20:06:18 +0100

    init

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,12 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
   trustHost: true,
   providers: [
     GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.AUTH_GITHUB_ID ?? process.env.GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET ?? process.env.GITHUB_SECRET!,
       authorization: {
         params: {
           scope: "repo read:user user:email",
